b9a229f846130e290d471568dd7a55186d37272b
hholler90 <104468878+hholler90@users.noreply.github.com>
2023-06-28 19:13:40 -0300

    Add files via upload

---
+++ HTML-CSS/index.js
@@ -0,0 +1,13 @@
+function abrirMenu(){
+    document.getElementById("aba").style.width = '15%';
+    document.getElementById("fundo").style.width='300vh';
+    document.getElementById("fundo").style.height='300vh';
+}
+function fecharMenu(){
+    document.getElementById("aba").style.width='0%';
+    document.getElementById("fundo").style.width='0%';
+}
+
+
+
+
